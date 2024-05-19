b0ed010a401b7b680310e06e0e5cd73ff84a1eea
jph9184 <jph9184@gmail.com>
2024-05-19 22:43:26 +0000

    Initial commit

---
+++ 01_set.py
@@ -0,0 +1,15 @@
+#Conjuntos
+#Los elementos repetidos se eliminan
+
+set_countries = {"Colombia", 'México', 'Bolivia'}
+print(set_countries)
+print(type(set_countries))
+
+set_numbers = {1, 2, 2, 2, 3}
+print(set_numbers)
+
+set_types = {1, 'hola', False, 12.12}
+print(set_types)
+
+set_from_string = set('hooooooola')
+print(set_from_string)
